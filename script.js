5fabc18403e928b25b130ba0c90d420bc3abd889
David Obaro <32646072+davidobaro@users.noreply.github.com>
2025-05-18 11:16:41 -0400

    ---

--- script.js
+++ script.js
@@ -6,23 +6,35 @@ function toggleMenu() {
 }
 
 const projectDescriptions = {
-  0: `Designed and implemented a Python-based analysis tool to evaluate 200+ Pittsburgh neighborhoods using public health and safety data from the Western Pennsylvania Regional Data Center. Processed datasets on housing quality, arrest rates, and median age at death to engineer features and calculate composite scores. Automated data cleaning, normalization, and ranking using pandas and NumPy. Delivered visual insights and scoring logic to support data-driven decision-making for community evaluation.`,
+  0: `Designed and implemented a Python-based analysis tool to evaluate 200+ Pittsburgh neighborhoods using public health and safety data from the Western Pennsylvania Regional Data Center. 
+  Processed datasets on housing quality, arrest rates, and median age at death to engineer features and calculate composite scores. Automated data cleaning, normalization, and ranking using pandas and NumPy. 
+  Delivered visual insights and scoring logic to support data-driven decision-making for community evaluation.`,
   1: `Built an iOS kiosk app for university CECs to streamline visitor
-              check-ins. Developed the frontend in SwiftUI and helped build REST
-              APIs with Flask. Used PostgreSQL for data storage and deployed the
-              full stack on Heroku. Collaborated in a team setting, focusing on
-              clean UI and reliable backend integration.`,
+  check-ins. 
+  Developed the frontend in SwiftUI and helped build REST
+  APIs with Flask. 
+  Used PostgreSQL for data storage and deployed the
+  full stack on Heroku. 
+  Collaborated in a team setting, focusing on
+  clean UI and reliable backend integration.`,
   2: `Developed a classic Snake game in MIPS assembly using the MARS
-              simulator. Implemented real-time keyboard input, dynamic screen
-              rendering, and game logic entirely at the instruction level.
-              Demonstrated proficiency in low-level programming, memory
-              management, and system I/O operations.`,
+  simulator. 
+  Implemented real-time keyboard input, dynamic screen
+  rendering, and game logic entirely at the instruction level.
+  Demonstrated proficiency in low-level programming, memory
+  management, and system I/O operations.`,
   3: `Developed a C program that hides and extracts secret messages in
-              .bmp image files using least significant bit (LSB) encoding.
-              Implemented custom file parsing, bitwise data manipulation, and
-              message recovery without altering the image’s appearance.
-              Showcases skills in low-level systems programming, file I/O, and
-              binary operations.`,
+  .bmp image files using least significant bit (LSB) encoding.
+  Implemented custom file parsing, bitwise data manipulation, and
+  message recovery without altering the image’s appearance.
+  Showcases skills in low-level systems programming, file I/O, and
+  binary operations.`,
+  4: `Collaborated with a partner to implement a sample rental system using Test Driven Development (TDD) principles and employing concepts from the SDLC.
+Developed and wrote unit tests for the classes using JUnit and Mockito, ensuring a 90% coverage for the codebase.`,
+  5: `----`,
+  6: `----`,
+  7: `----`,
+  8: `----`,
 };
 
 function openModal(index) {
